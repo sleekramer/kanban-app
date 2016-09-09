9f2a7fe4e7a0d54a70491ba432c4c014d7afae85
Steven Lee-Kramer <sleekramer@gmail.com>
2016-09-09 17:19:19 -0400

    complete basic note application setup with addNote functionality

---
+++ app/components/App.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import uuid from 'uuid';
+import Notes from './Notes';
+
+// const notes = [
+//   {
+//     id: uuid.v4(),
+//     task: 'Learn React'
+//   },
+//   {
+//     id: uuid.v4(),
+//     task: 'Do Laundry'
+//   }
+// ];
+//
+// export default () => (
+//   <div>
+//     <button onClick={() => console.log('add note')}>+</button>
+//     <Notes notes={notes} />
+//   </div>
+// );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do Laundry'
+        }
+      ]
+    };
+  }
+  render() {
+    const {notes} = this.state;
+
+    return (
+      <div>
+        <button onClick={this.addNote}>+</button>
+        <Notes notes={notes} />
+      </div>
+    );
+  }
+
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
+  }
+}
