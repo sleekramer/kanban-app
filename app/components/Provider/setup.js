118abc282f6f81cb0253d37f31cb5056dd510b9f
Steven Lee-Kramer <sleekramer@gmail.com>
2016-10-05 16:29:06 -0400

    add data persistency by utilizing localStorage

--- app/components/Provider/setup.js
+++ app/components/Provider/setup.js
@@ -1,5 +1,9 @@
+import storage from '../../lib/storage';
+import persist from '../../lib/persist';
 import NoteStore from '../../stores/NoteStore';
 
 export default alt => {
   alt.addStore('NoteStore', NoteStore);
+
+  persist(alt, storage(localStorage), 'app');
 };
